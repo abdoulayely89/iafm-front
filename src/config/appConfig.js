855d25892b9dc3ee124b7ecdbff93d2c8dac1c3d
Abdoulaye Ly <abdoulayely@192.168.1.25>
2025-12-02 13:22:11 +0000

    déploiement

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,5 +1,5 @@
 const appConfig = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5002/api',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://iafm-back-production.up.railway.app/api',
 }
 
 export default appConfig
